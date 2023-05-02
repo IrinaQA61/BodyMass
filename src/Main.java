ca5c9ff290bf752ae6fb4dfeb156ac5aa614fa95
Neto test <irina.plotnikova72@mail.ru>
2023-05-02 17:45:32 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         int weight = 66; // вес в килограммах
         double height = 1.64; // рост в метрах
         double bmi = service.calculate(weight, height); // считаем ИМТ
-        String bmiResult = String.format("%.2f",bmi); // округляем до 2-х знаков
+        String bmiResult = String.format("%.2f", bmi); // округляем до 2-х знаков
         System.out.println("Ваш вес: " + weight + " кг.");
-        System.out.println("Ваш рост: " + height*100 + " см.");
+        System.out.println("Ваш рост: " + height * 100 + " см.");
         System.out.println("Ваш индекс массы тела: " + bmiResult);
     }
 }
